0b9f7f22b2a154528b3c9acde897d77de28bd087
PhilipGuo1992 <philipguo1992@gmail.com>
2018-01-22 14:53:19 -0400

    write onClick function

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickSubmit(View view){
+        // get the user input
+        TextView textView = findViewById(R.id.editText);
+        String userInput = textView.getText().toString();
+        // Log.i("g", "clickSubmit: " + userInput);
+        //update the textbox
+        TextView textbox = findViewById(R.id.textBox);
+        textbox.setText(userInput);
+
+        // remove the input
+        textView.setText("");
+
     }
 }
